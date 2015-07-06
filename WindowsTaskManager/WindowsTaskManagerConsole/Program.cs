21be1d627ee1b4fb9e0b4df88a068097c092f6e5
Jan <rsheynin@gmail.com>
2015-07-06 13:47:05 +0300

    Process, Service, Application lists

--- WindowsTaskManager/WindowsTaskManagerConsole/Program.cs
+++ WindowsTaskManager/WindowsTaskManagerConsole/Program.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceProcess;
 
 namespace WindowsTaskManagerConsole
 {
@@ -10,6 +9,42 @@ namespace WindowsTaskManagerConsole
     {
         static void Main(string[] args)
         {
+            Process[] processes = Process.GetProcesses();
+            var runningAppCount = 0;
+            foreach (var process in processes)
+            {
+                if (!String.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    
+                    Console.WriteLine(process.ProcessName);
+                    Console.WriteLine(process.MainWindowTitle);
+                    Console.WriteLine("######################");
+                    runningAppCount++;
+                }
+            }
+            Console.WriteLine("runningAppCount : {0}", runningAppCount);
+
+
+
+            foreach (var theprocess in processes)
+            {
+                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+            }
+            Console.WriteLine("processesCount : {0}", processes.Count());
+
+
+            // get list of Windows services
+            var services = ServiceController.GetServices();
+
+            // try to find service name
+            foreach (var service in services)
+            {
+                Console.WriteLine(service.ServiceName);
+            }
+            Console.WriteLine("servicesCount : {0}", services.Count());
+
+
+            Console.ReadKey();
         }
     }
 }
